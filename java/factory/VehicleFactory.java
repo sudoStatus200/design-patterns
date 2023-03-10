4e6a33b971b7cfbc5c7e3134d61df1e434cc5a15
rahulbreezo <rahul@breezo.in>
2023-03-10 22:53:25 +0530

    complete factory pattern

---
+++ java/factory/VehicleFactory.java
@@ -0,0 +1,48 @@
+package java.factory;
+
+abstract class Vehicle {
+    public abstract int getWheelCount();
+
+    public String toString(){
+        return "Wheel count:"+ this.getWheelCount()
+    }
+}
+
+class Car extends Vehicle {
+    Car() {
+    }
+
+    @Override
+    public int getWheelCount() {
+        return 4;
+    }
+}
+
+class Bike extends Vehicle {
+    Bike() {
+    }
+
+    @Override
+    public int getWheelCount() {
+        return 4;
+    }
+}
+
+class Factory {
+    public static Vehicle getVehicle(String type) {
+        if (type == "Car") {
+            return new Car();
+        }
+        if (type == "Bike") {
+            return new Bike();
+        }
+        return null;
+    }
+}
+
+public class VehicleFactory {
+    public static void main(String[] args) {
+        Vehicle car = Factory.getVehicle("Car");
+        System.out.println(car);
+    }
+}
